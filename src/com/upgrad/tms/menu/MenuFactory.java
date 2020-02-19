af5f808ed1c48c1ebbc7522410f8f061d53b6d67
ishwar-soni <59160606+ishwar-soni@users.noreply.github.com>
2020-02-19 20:24:00 +0530

    MenuFactory
    created
      -MenuFactory
    
    added
      -main method in MainMenu
      -showTopOptions() in ManagerMenu

---
+++ src/com/upgrad/tms/menu/MenuFactory.java
@@ -0,0 +1,13 @@
+package com.upgrad.tms.menu;
+
+public class MenuFactory {
+    public static OptionsMenu getMenuByType(OptionsMenuType optionsMenuType) {
+        switch (optionsMenuType){
+            case PROJECT_MANAGER:
+                return new ManagerMenu();
+            case ASSIGNEE:
+                return new AssigneeMenu();
+        }
+        return null;
+    }
+}
