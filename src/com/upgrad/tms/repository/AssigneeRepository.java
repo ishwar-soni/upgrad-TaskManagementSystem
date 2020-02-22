70a4402c7eb48c30cec1ac2bad8bbbe27c9b3dcc
ishwar-soni <59160606+ishwar-soni@users.noreply.github.com>
2020-02-22 14:39:41 +0530

    Adding AssigneeList and AssigneeRepository

---
+++ src/com/upgrad/tms/repository/AssigneeRepository.java
@@ -0,0 +1,16 @@
+package com.upgrad.tms.repository;
+
+import com.upgrad.tms.util.AssigneeList;
+
+public class AssigneeRepository {
+    private AssigneeList assigneeList;
+    private static AssigneeRepository assigneeRepository;
+
+    private AssigneeRepository() {
+        initAssignee();
+    }
+
+    private void initAssignee(){
+        
+    }
+}
