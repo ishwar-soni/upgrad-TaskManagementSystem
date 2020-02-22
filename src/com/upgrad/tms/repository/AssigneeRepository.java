4d35a74aec5d3c2ec711158512753255033a3de0
ishwar-soni <59160606+ishwar-soni@users.noreply.github.com>
2020-02-22 15:38:26 +0530

    Creating ObjectOutputStream

--- src/com/upgrad/tms/repository/AssigneeRepository.java
+++ src/com/upgrad/tms/repository/AssigneeRepository.java
@@ -1,5 +1,7 @@
 package com.upgrad.tms.repository;
 
+import com.upgrad.tms.entities.Assignee;
+import com.upgrad.tms.exception.AssigneeListFullException;
 import com.upgrad.tms.util.AssigneeList;
 
 import java.io.*;
@@ -13,14 +15,34 @@ public class AssigneeRepository {
     }
 
     private void initAssignee() throws IOException, ClassNotFoundException {
-        FileInputStream fi = new FileInputStream(new File("assignee.txt"));
-        if (fi.available() > 0) {
-            ObjectInputStream oi = new ObjectInputStream(fi);
-            assigneeList = (AssigneeList) oi.readObject();
-            oi.close();
-        } else {
-            assigneeList = new AssigneeList();
+        try (
+            FileInputStream fi = new FileInputStream(new File("assignee.txt"))
+        ) {
+            if (fi.available() > 0) {
+                ObjectInputStream oi = new ObjectInputStream(fi);
+                assigneeList = (AssigneeList) oi.readObject();
+                oi.close();
+            } else {
+                assigneeList = new AssigneeList();
+            }
         }
-        fi.close();
+    }
+
+    public Assignee saveAssignee(Assignee assignee) throws AssigneeListFullException {
+        assigneeList.add(assignee);
+        updateListToFile();
+        return assignee;
+    }
+
+    public void updateListToFile() {
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(new File("assignee.txt"));
+            ObjectOutputStream outputStream = new ObjectOutputStream(fileOutputStream);
+            outputStream.writeObject(assigneeList);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
