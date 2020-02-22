ae5d861e5a1668c65caf476d93610040823194e4
ishwar-soni <59160606+ishwar-soni@users.noreply.github.com>
2020-02-22 21:55:26 +0530

    implementing ManagerRepository

--- src/com/upgrad/tms/repository/ManagerRepository.java
+++ src/com/upgrad/tms/repository/ManagerRepository.java
@@ -1,6 +1,10 @@
 package com.upgrad.tms.repository;
 
+import com.upgrad.tms.exception.ManagerMapFullException;
+import com.upgrad.tms.util.Constants;
 import com.upgrad.tms.util.ManagerMap;
+
+import java.io.*;
 
 public class ManagerRepository {
     private static ManagerRepository managerRepository;
@@ -18,6 +22,33 @@ public class ManagerRepository {
     }
 
     private void initManagerCredentials() {
-        
+        managerCredentials = new ManagerMap();
+        try (BufferedReader br = new BufferedReader(new FileReader(Constants.MANAGER_FILE_NAME))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] split = line.split(",");
+                managerCredentials.put(split[0], split[1]);
+            }
+        } catch (IOException e) {
+            System.out.println("IO Exception handling");
+            e.printStackTrace();
+        } catch (ManagerMapFullException e) {
+            System.out.println(e.getMessage());
+        }
     }
+
+    private void saveManager(String userName, String password) {
+        BufferedWriter writer;
+        try {
+            writer = new BufferedWriter(new FileWriter(Constants.MANAGER_FILE_NAME, true));
+            String line = userName+","+password;
+            writer.newLine();
+            writer.write(line);
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("IO Exception handling");
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/com/upgrad/tms/util/Constants.java
+++ src/com/upgrad/tms/util/Constants.java
@@ -2,4 +2,5 @@ package com.upgrad.tms.util;
 
 public class Constants {
     public static final String ASSIGNEE_FILE_NAME="assignee.txt";
+    public static final String MANAGER_FILE_NAME="manager.txt";
 }
