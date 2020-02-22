8139a4d5aab46070db3aa44bff85a32fc74a7cec
ishwar-soni <59160606+ishwar-soni@users.noreply.github.com>
2020-02-22 15:16:01 +0530

    implementing intiAssignee() of AssigneeRepository

--- src/com/upgrad/tms/repository/AssigneeRepository.java
+++ src/com/upgrad/tms/repository/AssigneeRepository.java
@@ -2,15 +2,25 @@ package com.upgrad.tms.repository;
 
 import com.upgrad.tms.util.AssigneeList;
 
+import java.io.*;
+
 public class AssigneeRepository {
     private AssigneeList assigneeList;
     private static AssigneeRepository assigneeRepository;
 
-    private AssigneeRepository() {
+    private AssigneeRepository() throws IOException, ClassNotFoundException {
         initAssignee();
     }
 
-    private void initAssignee(){
-        
+    private void initAssignee() throws IOException, ClassNotFoundException {
+        FileInputStream fi = new FileInputStream(new File("assignee.txt"));
+        if (fi.available() > 0) {
+            ObjectInputStream oi = new ObjectInputStream(fi);
+            assigneeList = (AssigneeList) oi.readObject();
+            oi.close();
+        } else {
+            assigneeList = new AssigneeList();
+        }
+        fi.close();
     }
 }
