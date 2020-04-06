409def9e00583ee857bdf56663601b743e541b17
ishwar-soni <59160606+ishwar-soni@users.noreply.github.com>
2020-04-06 20:13:11 +0530

    Using CyclicBarrier

--- src/com/upgrad/tms/countdownlatch/ChildWorker.java
+++ src/com/upgrad/tms/countdownlatch/ChildWorker.java
@@ -4,21 +4,34 @@ import com.upgrad.tms.entities.Task;
 import com.upgrad.tms.menu.AbstractWorker;
 import com.upgrad.tms.repository.AssigneeRepository;
 
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 
 public class ChildWorker extends AbstractWorker implements Runnable {
 
     private final CountDownLatch countDownLatch;
     private final Task task;
+    private final CyclicBarrier cyclicBarrier;
 
-    public ChildWorker(AssigneeRepository assigneeRepository, Task task, CountDownLatch countDownLatch) {
+    public ChildWorker(AssigneeRepository assigneeRepository, Task task, CountDownLatch countDownLatch, CyclicBarrier cyclicBarrier) {
         super(assigneeRepository);
         this.countDownLatch = countDownLatch;
         this.task = task;
+        this.cyclicBarrier = cyclicBarrier;
     }
 
     @Override
     public void doWork() {
+        System.out.println("Task id: "+task.getId());
+        System.out.println("Cyclic barrier total parties: "+cyclicBarrier.getParties()+ " Waiting parties: "+cyclicBarrier.getNumberWaiting());
+        try {
+            cyclicBarrier.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
         processTask(task);
         updateInFile();
         countDownLatch.countDown();
--- src/com/upgrad/tms/menu/AssigneeMenu.java
+++ src/com/upgrad/tms/menu/AssigneeMenu.java
@@ -21,6 +21,7 @@ import com.upgrad.tms.util.TaskStatus;
 import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.Semaphore;
 import java.util.stream.Collectors;
 
@@ -104,10 +105,16 @@ public class AssigneeMenu implements OptionsMenu {
         List<Task> multipleTask = getMultipleTask();
         if (multipleTask.size() > 2) {
             CountDownLatch countDownLatch = new CountDownLatch(multipleTask.size()- 1);
+            CyclicBarrier cyclicBarrier = new CyclicBarrier(multipleTask.size() - 1 );
             Thread thread = new Thread(new ParentWorker(assigneeRepository, countDownLatch, multipleTask.get(0)));
             thread.start();
             for (int i = 1; i < multipleTask.size(); i++) {
-                new Thread(new ChildWorker(assigneeRepository, multipleTask.get(i), countDownLatch)).start();
+                new Thread(new ChildWorker(assigneeRepository, multipleTask.get(i), countDownLatch, cyclicBarrier)).start();
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
 
